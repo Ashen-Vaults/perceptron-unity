2b24414cfdfeaac96e83e30a65c8e2a4ffd05d80
Ashen-Vaults <alexhollander522@gmail.com>
2018-02-01 20:05:12 -0500

    Perceptron weight functions

--- Assets/Code/Perceptron.cs
+++ Assets/Code/Perceptron.cs
@@ -1,9 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Perceptron : MonoBehaviour 
+/*
+
+    * For every input, multiply that input by its weight.
+
+    * Sum all of the weighted inputs.
+    
+    * Compute the output of the perceptron based on the sum pass thru an
+      activation function (the sign of the sum)
+
+ */
+ [Serializable]
+public class Perceptron 
 {
+    [SerializeField]
+    List<float> _weights = new List<float>();
+
+    public Perceptron(int weightSize)
+    {
+        //Initialize the weights
+        for (int i=0; i < weightSize; i++)
+        {
+            _weights.Add(UnityEngine.Random.Range(-1,1));
+        }
+    }
+
+    public int Guess(List<float> inputs)
+    {
+        float sum = 0;
+        for (int i = 0; i < _weights.Count; i++)
+        {
+            sum += inputs[i] * _weights[i];   
+        }
+        return Math.Sign(sum);
+    }
 
 }
  
---
+++ Assets/Code/World.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour 
+{
+
+    [SerializeField]
+    Perceptron _perceptron;
+
+    [SerializeField]
+    List<float> _inputs;
+
+    void Awake()
+    {
+        _perceptron = new Perceptron(2);
+        Debug.Log(_perceptron.Guess(_inputs));
+    }   
+
+
+}
+ 

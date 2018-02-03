ebb30a4626bd53d7ad761c7360085a645fb38173
Ashen-Vaults <alexhollander522@gmail.com>
2018-02-02 20:54:02 -0500

    Created math extensions
    Needed a function to remap one range to another

---
+++ Assets/Code/Utility/MathExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace AshenCode.Extensions
+{
+
+    public static class MathExtensions
+    {
+
+        public static float Map (this float value, float from1, float to1, float from2, float to2)
+        {
+            return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+        }
+
+
+    }
+
+} 
